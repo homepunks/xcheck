aec8d1ef9a16ee55601b5065c5e88960e9adf52d
homepunks <wispenjoyer@gmail.com>
2026-07-14 21:39:07 +0500

    feat: accept currency options from user

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use serde::Deserialize;
+use clap::Parser;
 
 #[derive(Deserialize, Debug)]
 struct Resp {
@@ -8,9 +9,21 @@ struct Resp {
     rate: f32,
 }
 
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[arg(short, long, default_value = "USD")]
+    from: String,
+
+    #[arg(short, long, default_value = "KZT")]
+    to: String,
+}
+
 fn main() -> anyhow::Result<()> {
+    let args = Cli::parse();
+
     let base_url = "https://api.frankfurter.dev/v2/rates";
-    let url = format!("{}?base={}&quotes={}", base_url, "USD", "KZT");
+    let url = format!("{}?base={}&quotes={}", base_url, args.from, args.to);
     let mut resp: Vec<Resp> = reqwest::blocking::get(url)?.json()?;
     let resp = resp.remove(0);
 
