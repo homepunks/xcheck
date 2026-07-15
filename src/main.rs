b1cd9aebe1a1fe206a0a1a19aef80fa362eed79e
homepunks <wispenjoyer@gmail.com>
2026-07-15 17:44:59 +0500

    feat: additional help msg for currencies < 1KZT

--- src/main.rs
+++ src/main.rs
@@ -25,6 +25,7 @@ mod ansi {
     pub const GRAY: &str = "\x1b[38;2;168;153;132m";
     pub const GREEN: &str = "\x1b[38;2;184;187;38m";
     pub const YELLOW: &str = "\x1b[38;2;250;189;47m";
+    pub const AQUA: &str = "\x1b[38;2;142;192;124m";
 }
 
 fn main() -> anyhow::Result<()> {
@@ -41,7 +42,7 @@ fn main() -> anyhow::Result<()> {
         .map_err(|_| anyhow::anyhow!("expected exactly one rate"))?;
 
     println!(
-        "{gray}[{date}]{reset} {yellow}{base}/{quote}{reset} {gray}→{reset} {green}{rate:.2}{reset}",
+        "{gray}[{date}]{reset} {yellow}{base}/{quote}{reset} {gray}->{reset} {green}{rate:.2}{reset}",
         gray = ansi::GRAY,
         yellow = ansi::YELLOW,
         green = ansi::GREEN,
@@ -51,6 +52,16 @@ fn main() -> anyhow::Result<()> {
         quote = resp.quote,
         rate = resp.rate,
     );
+
+    if resp.rate < 1.0 && &to == "KZT" {
+        println!("{aqua}1 {quote} is precisely {rate:.2} {base}!{reset}",
+            aqua = ansi::AQUA,
+            quote = resp.quote,
+            rate = 1.0 / resp.rate,
+            base = resp.base,
+            reset = ansi::RESET,
+        );
+    }
 
     Ok(())
 }
