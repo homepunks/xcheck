793253f9e338f6d72728c378428d338956a06658
homepunks <wispenjoyer@gmail.com>
2026-07-16 11:34:26 +0500

    feat: stat sub-cmd for plotting continuous data

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use clap::Parser;
+use clap::{Args, Parser, Subcommand};
 use serde::Deserialize;
+use chrono::{Local, Duration};
+use textplots::{Chart, Shape, Plot};
 
 #[derive(Deserialize, Debug)]
 struct Resp {
@@ -12,11 +14,25 @@ struct Resp {
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Cli {
+    #[command(subcommand)]
+    command: Option<Command>,
+
+    #[command(flatten)]
+    currencies: Currencies,
+}
+
+#[derive(Args)]
+struct Currencies {
     #[arg(short, long, default_value = "USD")]
     from: String,
 
     #[arg(short, long, default_value = "KZT")]
     to: String,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    Stat { days: u32 },
 }
 
 mod ansi {
@@ -29,38 +45,57 @@ mod ansi {
 }
 
 fn main() -> anyhow::Result<()> {
-    let args = Cli::parse();
-    let from = args.from.to_uppercase();
-    let to = args.to.to_uppercase();
-
     let base_url = "https://api.frankfurter.dev/v2/rates";
-    let url = format!("{}?base={}&quotes={}", base_url, from, to);
-    let [resp]: [Resp; 1] = reqwest::blocking::get(url)?
-        .error_for_status()?
-        .json::<Vec<Resp>>()?
-        .try_into()
-        .map_err(|_| anyhow::anyhow!("expected exactly one rate"))?;
+    let args = Cli::parse();
+    let from = args.currencies.from.to_uppercase();
+    let to = args.currencies.to.to_uppercase();
 
-    println!(
-        "{gray}[{date}]{reset} {yellow}{base}/{quote}{reset} {gray}->{reset} {green}{rate:.2}{reset}",
-        gray = ansi::GRAY,
-        yellow = ansi::YELLOW,
-        green = ansi::GREEN,
-        reset = ansi::RESET,
-        date = resp.date,
-        base = resp.base,
-        quote = resp.quote,
-        rate = resp.rate,
-    );
+    match args.command {
+        Some(Command::Stat { days }) => {
+            let now    = Local::now().date_naive();
+            let since  = now - Duration::days(days as i64);
+            let url = format!("{}?from={}&base={}&quotes={}", base_url, since.format("%Y-%m-%d"), from, to);
+            let resp = reqwest::blocking::get(url)?
+                .error_for_status()?
+                .json::<Vec<Resp>>()?;
+  
+            let rates: Vec<_> = resp.iter().map(|r| r.rate).collect();
+            let points: Vec<_> = rates.into_iter().enumerate().map(|(i, rate)| (i as f32, rate as f32)).collect();
 
-    if resp.rate < 1.0 && &to == "KZT" {
-        println!("{aqua}1 {quote} is precisely {rate:.2} {base}!{reset}",
-            aqua = ansi::AQUA,
-            quote = resp.quote,
-            rate = 1.0 / resp.rate,
-            base = resp.base,
-            reset = ansi::RESET,
-        );
+            let xmax = points.len().saturating_sub(1) as f32;
+            Chart::new(180, 60, 0.0, xmax).lineplot(&Shape::Lines(&points)).nice();
+        }
+        None => {
+            let url = format!("{}?base={}&quotes={}", base_url, from, to);
+            let [resp]: [Resp; 1] = reqwest::blocking::get(url)?
+                .error_for_status()?
+                .json::<Vec<Resp>>()?
+                .try_into()
+                .map_err(|_| anyhow::anyhow!("expected exactly one rate"))?;
+
+            println!(
+                "{gray}[{date}]{reset} {yellow}{base}/{quote}{reset} {gray}->{reset} {green}{rate:.2}{reset}",
+                gray = ansi::GRAY,
+                yellow = ansi::YELLOW,
+                green = ansi::GREEN,
+                reset = ansi::RESET,
+                date = resp.date,
+                base = resp.base,
+                quote = resp.quote,
+                rate = resp.rate,
+            );
+
+            if resp.rate < 1.0 && &to == "KZT" {
+                println!(
+                    "{aqua}1 {quote} is precisely {rate:.2} {base}!{reset}",
+                    aqua = ansi::AQUA,
+                    quote = resp.quote,
+                    rate = 1.0 / resp.rate,
+                    base = resp.base,
+                    reset = ansi::RESET,
+                );
+            }
+        }
     }
 
     Ok(())
